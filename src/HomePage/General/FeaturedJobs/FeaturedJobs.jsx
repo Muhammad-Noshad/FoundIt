0f00f70be06adf57e7d5d9be7160be134a6dfe51
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-12-14 16:06:10 +0500

    Fixed Featured Jobs Bug

--- src/HomePage/General/FeaturedJobs/FeaturedJobs.jsx
+++ src/HomePage/General/FeaturedJobs/FeaturedJobs.jsx
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 
 import PostedJobCard from "../../../Cards/JobSeeker/PostedJobCard/PostedJobCard";
 import postedJobStore from "../../../Store/postedJobStore";
+import ManageJobPostCard from "../../../Cards/JobPoster/ManageJobPostCard/ManageJobPostCard";
+import userStore from "../../../Store/userStore";
 
 const FeaturedJobs = ({ title, subtitle, linkTo }) => {
   const postedJobs = postedJobStore((state) => state.postedJobs);
+  const user = userStore((state) => state.user);
 
   return (
     <section className="featured-jobs">
@@ -16,18 +19,36 @@ const FeaturedJobs = ({ title, subtitle, linkTo }) => {
         <div className="job-cards">
           {
             postedJobs.length?
-            postedJobs.slice(0, 3).map((value, index) =>
-              <PostedJobCard 
-                jobId={value.jobId}
-                jobTitle={value.jobTitle} 
-                jobType={value.jobType}
-                jobSalary={value.jobSalary}
-                jobDescription={value.jobDescription}
-                companyName={value.company.companyName}
-                companyLocation={value.company.companyLocation}
-                companyLogo={value.company.companyLogo}
-                key={index}
-              />
+            postedJobs.slice(0, 3).map((value, index) => {
+              if(user?.role === "Employer") {
+                return (
+                  <ManageJobPostCard 
+                    jobId={value.jobId}
+                    jobTitle={value.jobTitle} 
+                    jobType={value.jobType}
+                    jobSalary={value.jobSalary}
+                    jobDescription={value.jobDescription}
+                    companyName={value.company.companyName}
+                    companyLocation={value.company.companyLocation}
+                    companyLogo={value.company.companyLogo}
+                    key={index}
+                  />
+                )
+              }
+              else if(user?.role === "JobSeeker") {
+                <PostedJobCard 
+                  jobId={value.jobId}
+                  jobTitle={value.jobTitle} 
+                  jobType={value.jobType}
+                  jobSalary={value.jobSalary}
+                  jobDescription={value.jobDescription}
+                  companyName={value.company.companyName}
+                  companyLocation={value.company.companyLocation}
+                  companyLogo={value.company.companyLogo}
+                  key={index}
+                />
+              }
+            }
             )
             :
             <p className="dark no-jobs">No jobs to show!</p>
